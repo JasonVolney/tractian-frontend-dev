ae8c9d4e6f8fa18d849c6ce34ba2dace7124f348
JasonVolney <jasvol@gmail.com>
2021-12-06 18:33:02 -0300

    adjust layout

--- src/helpers/Constants.jsx
+++ src/helpers/Constants.jsx
@@ -1,2 +1,2 @@
-// export const JSON_API = 'https://tractian-frontend-dev.herokuapp.com/api';
-export const JSON_API = 'http://localhost:8000';
+export const JSON_API = 'https://tractian-frontend-dev.herokuapp.com/api';
+// export const JSON_API = 'http://localhost:8000';
